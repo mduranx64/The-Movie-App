72af90bab2bf6995a7f2328dd5a81dc710b1f5dc
MIGUEL DURAN <mdurantv@gmail.com>
2021-01-17 11:45:57 -0300

    Setup project

---
+++ TheMovieApp/TheMovieApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  TheMovieApp
+//
+//  Created by Miguel Duran on 17-01-21.
+//  Copyright © 2021 mduranx64. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
