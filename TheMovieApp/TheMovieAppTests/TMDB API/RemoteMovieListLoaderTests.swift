6e302ddd44e7c4b29e85b9f46cc2c7605c3fa6d7
MIGUEL DURAN <mdurantv@gmail.com>
2021-01-18 23:55:08 -0300

    Check that RemoteMovieListLoader return correct client error

--- TheMovieApp/TheMovieAppTests/TMDB API/RemoteMovieListLoaderTests.swift
+++ TheMovieApp/TheMovieAppTests/TMDB API/RemoteMovieListLoaderTests.swift
@@ -35,6 +35,26 @@ class RemoteMovieListLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_returnErrorOnClientError() {
+        let (sut, client) = makeSUT()
+        let clientError = NSError(domain: "Test", code: 0)
+        let exp = expectation(description: "wait for load completion")
+        
+        sut.load { result in
+            switch result {
+            case let .failure(receivedError as RemoteMovieListLoader.MovieError):
+                XCTAssertEqual(receivedError, RemoteMovieListLoader.MovieError.connectivity)
+            default:
+                XCTFail()
+            }
+            
+            exp.fulfill()
+        }
+        
+        client.complete(with: clientError)
+        wait(for: [exp], timeout: 1.0)
+    }
+    
     private func makeSUT(url: URL = URL(string: "https://any-url.com")!) -> (sut: RemoteMovieListLoader, client: HTTPClientSpy) {
         let client = HTTPClientSpy()
         let sut = RemoteMovieListLoader(url: url, client: client)
@@ -48,11 +68,20 @@ class HTTPClientSpy: HTTPClient {
             
         }
     }
+    
+    private var messages = [(url: URL, completion: (Result<(Data, HTTPURLResponse), Error>) -> Void)]()
+    
         
-    var requestedURLs = [URL]()
+    var requestedURLs: [URL] {
+        return messages.map { $0.url }
+    }
     
     func get(from url: URL, completion: @escaping (Result<(Data, HTTPURLResponse), Error>) -> Void) -> HTTPClientTask {
-        requestedURLs.append(url)
+        messages.append((url, completion))
         return Task()
+    }
+    
+    func complete(with error: Error, at index: Int = 0) {
+        messages[index].completion(.failure(error))
     }
 }
