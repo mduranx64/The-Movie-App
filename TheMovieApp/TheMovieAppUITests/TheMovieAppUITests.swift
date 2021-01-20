c4537e04d2b4ee3c8f87199a8874932fd238ba8a
MIGUEL DURAN <mdurantv@gmail.com>
2021-01-20 17:25:41 -0300

    Test navigation from main view to details view and back button

--- TheMovieApp/TheMovieAppUITests/TheMovieAppUITests.swift
+++ TheMovieApp/TheMovieAppUITests/TheMovieAppUITests.swift
@@ -10,6 +10,20 @@ import XCTest
 
 class TheMovieAppUITests: XCTestCase {
 
+    override func setUp() {
+        continueAfterFailure = false
+        
+        XCUIApplication().launch()
+    }
+    
+    func test_navigationToDetailsMovieView() {
  
+        let app = XCUIApplication()
+        app.collectionViews.children(matching: .any).element(boundBy: 0).tap()
+        app.navigationBars["The Movie App"].buttons["The Movie App"].tap()
+  
+        let cell = app.collectionViews.children(matching: .any).element(boundBy: 0)
+        XCTAssertTrue(cell.exists)
+    }
 
 }
