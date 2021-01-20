025bfd3717659b9c35310986ba28fdf158a82296
MIGUEL DURAN <mdurantv@gmail.com>
2021-01-20 13:08:27 -0300

    Add movie details view layout

--- TheMovieApp/TheMovieApp/Movie UI/MovieDetailsViewController.swift
+++ TheMovieApp/TheMovieApp/Movie UI/MovieDetailsViewController.swift
@@ -13,6 +13,8 @@ public class MovieDetailsViewController: UIViewController {
     private let movie: Movie
     private let posterImage: UIImage?
     
+    private let movieDetailsView = MovieDetailsView()
+    
     public init(movie: Movie, posterImage: UIImage?) {
         self.movie = movie
         self.posterImage = posterImage
@@ -21,5 +23,10 @@ public class MovieDetailsViewController: UIViewController {
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view = movieDetailsView
     }
 }
